746138f3c8f1056ad45df791f2f6c89a7f0f13ad
jtwray <tuckerwray84@gmail.com>
2019-06-25 20:15:33 -0400

    Creates stateful class component App. App holds initial state of friends as an empty array. imports Axios, on Lifecycle method CDM calls GET method with AXIOS to localhost port 5000/friends. logs array of 6 friend object to the console.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
-
+class App extends React.Component {
+	state = {
+		friends: []
+	}
+  componentDidMount() {
+		axios.get('http://localhost:5000/friends/' )
+		.then(response => {
+			this.setState(response.data)
+		} )
+			.then( () => axios.get( 'http://localhost:5000/friends/' ) )
+			.then( response => {
+				console.log(response.data)
+			})
+			.catch( err => {
+			console.log('Error:', err)
+		})
+	
+  }
+  render() {
+    
+    return (
+      <div className="App">
+      
+      </div>
+      );
+    }
+    
+  }
 export default App;
